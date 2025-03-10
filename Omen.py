6f6062e818beaeb95f53a8c76eb6d150ec2195af
Manigandan Nachiappan <128712162+manigandan-2003@users.noreply.github.com>
2025-03-10 20:07:54 +0530

    Update Omen.py

--- Omen.py
+++ Omen.py
@@ -7,7 +7,7 @@ prime_flag = 0
 
 if(n > 1):
     for i in range(2, int(sqrt(n)) + 1):
-        if (n % i == 0):
+        if (n / i == 0):
             prime_flag = 1
             break
     if (prime_flag == 0):
